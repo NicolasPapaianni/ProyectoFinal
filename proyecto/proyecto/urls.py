011543b6426955e9ab383db849dd180c1731374c
Nicolas Papaianni <papaianninicolas@gmail.com>
2022-08-07 16:16:10 +0200

    modificaciones en el home y nav bar

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -1,13 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from proyecto.views import bienvenida, template_bienvenida
+from proyecto.views import bienvenida, template_home
 from products.views import create_product, products_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bienvenida/', bienvenida, name = 'bienvenida'),
-    path('template_bienvenida/', template_bienvenida, name = 'template_bienvenida' ),
+    path('template_home/', template_home, name = 'template_home' ),
     path('create_product/', create_product, name = 'create_product'),
     path('products_list/', products_list, name = 'products_list' )
 ]
--- proyecto/proyecto/views.py
+++ proyecto/proyecto/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 def bienvenida(request):
     return HttpResponse('Bienvenidos a The House of Beer')
 
-def template_bienvenida(request):
-    return render(request, 'template_bienvenida.html', context= {})
+def template_home(request):
+    return render(request, 'template_home.html', context= {})
